8684f569baf10c4ffb680fa645a7826ef51de35a
enpitsulin <enpitsulin@gmail.com>
2023-01-28 11:27:12 +0800

    feat: create universal vue ui library template

---
+++ packages/build-v2/build.js
@@ -0,0 +1,23 @@
+import vue from '@vitejs/plugin-vue2';
+import { createRequire } from 'module';
+import { resolve } from 'path';
+import { build } from 'vite';
+
+const require = createRequire(import.meta.url);
+
+build({
+  plugins: [vue()],
+  build: {
+    outDir: resolve('../../dist/v2'),
+    rollupOptions: {
+      external: ['vue', 'vue-demi']
+    },
+    lib: { name: 'index', entry: require.resolve('@scope/components'), fileName: 'index' }
+  },
+  resolve: {
+    alias: {
+      vue: resolve('./node_modules/vue/dist/vue.runtime.esm-browser.js'),
+      'vue-demi': '../node_modules/vue-demi/lib/v3/index.mjs'
+    }
+  }
+});
---
+++ packages/components/button/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { ref } from 'vue-demi';
+  const count = ref(0);
+</script>
+<template>
+  <button class="button" @click="count++">
+    <span>count {{ count }}</span>
+  </button>
+</template>

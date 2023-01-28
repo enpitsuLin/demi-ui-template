5a0d4da8a0f2cae70c950779196d0b52466421a0
enpitsulin <enpitsulin@gmail.com>
2023-01-28 15:39:15 +0800

    build: entry file extname

--- packages/scope-ui/index.mjs
+++ packages/scope-ui/index.mjs
@@ -4,7 +4,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 if (isVue2) {
-  module.exports = require('./dist/v2/index.js');
+  module.exports = require('./dist/v2/index.mjs');
 } else {
-  module.exports = require('./dist/v3/index.js');
+  module.exports = require('./dist/v3/index.mjs');
 }

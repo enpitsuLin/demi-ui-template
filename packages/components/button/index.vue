46b0faad3d9d6eba642df44a15d71bcf0ec18043
enpitsulin <enpitsulin@gmail.com>
2023-01-30 12:18:18 +0800

    test: improved test with button example component

--- packages/components/button/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { ref } from 'vue-demi'
-const count = ref(0)
-</script>
-<template>
-  <button class="button" @click="count++">
-    <span>count {{ count }}</span>
-  </button>
-</template>
---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { buttonProps } from './button'
+defineProps(buttonProps)
+</script>
+<script lang="ts">
+export default {
+  name: 'DButton',
+}
+</script>
+<template>
+  <button class="d-button" :class="`d-button--${type}`">
+    <slot></slot>
+  </button>
+</template>

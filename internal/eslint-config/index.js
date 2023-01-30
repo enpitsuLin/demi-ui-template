484e08b3df847dc734853bd937225776f507b987
enpitsulin <enpitsulin@gmail.com>
2023-01-30 11:02:00 +0800

    feat: add eslint and prettier

---
+++ internal/eslint-config/index.js
@@ -0,0 +1,46 @@
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        'no-undef': 'off'
+      }
+    },
+    {
+      files: ['**/__test__/**'],
+      rules: {
+        'no-console': 'off'
+      }
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      rules: {
+        'no-undef': 'off'
+      }
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
+});

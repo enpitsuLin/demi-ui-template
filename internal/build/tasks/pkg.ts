c41722738cb7341ccc38a4911b853fefbaca70aa
enpitsulin <enpitsulin@gmail.com>
2023-01-30 14:00:07 +0800

    build: copy style to build product output

--- internal/build/tasks/pkg.ts
+++ internal/build/tasks/pkg.ts
@@ -1,6 +1,7 @@
-import { libOutput, libRoot } from '@demi-ui/build-utils'
+import { componentsRoot, libOutput, libRoot } from '@demi-ui/build-utils'
 import { copySync, readdir } from 'fs-extra'
 import path from 'path'
+import glob from 'fast-glob'
 
 export const copyFiles = async () => {
   const files = await readdir(libRoot)
@@ -9,5 +10,20 @@ export const copyFiles = async () => {
     files.map((file) =>
       copySync(path.resolve(libRoot, file), path.resolve(libOutput, file))
     )
+  )
+}
+
+export const copyFullStyle = async () => {
+  const styleFiles = await glob('**/*.css', {
+    cwd: componentsRoot,
+  })
+
+  return Promise.all(
+    styleFiles
+      .map((fpath) => [
+        path.resolve(componentsRoot, fpath),
+        path.resolve(libOutput, 'styles', fpath.replace(/\/style/, '')),
+      ])
+      .map(([src, dest]) => copySync(src, dest))
   )
 }

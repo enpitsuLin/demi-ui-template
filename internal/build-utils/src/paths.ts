32a185a9ff9bfb3002457fc74cbe8afe2b8c2371
enpitsulin <enpitsulin@gmail.com>
2023-01-30 15:49:10 +0800

    chore: add changesets to manage version

--- internal/build-utils/src/paths.ts
+++ internal/build-utils/src/paths.ts
@@ -10,7 +10,7 @@ export const buildRoot = resolve(workspace, 'internal', 'build')
 
 /** `/dist` */
 export const buildOutput = resolve(workspace, 'dist')
-/** `/dist/demi-ui` */
-export const libOutput = resolve(buildOutput, 'demi-ui')
+/** `/packages/demi-ui/release` */
+export const libOutput = resolve(libRoot, 'release')
 
 export const libPackage = resolve(libRoot, 'package.json')

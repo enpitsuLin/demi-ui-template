cae35151fac9725d776bd7fc9ac1be2752fed83f
enpitsulin <enpitsulin@gmail.com>
2023-01-30 17:17:04 +0800

    feat: find true vue-demi entry during build

--- internal/build/tasks/vite.ts
+++ internal/build/tasks/vite.ts
@@ -6,25 +6,41 @@ import { resolve } from 'path'
 import { build, type Plugin } from 'vite'
 import { createTask } from '../utils'
 
+const vueDemiEntry = (version: 2 | 3) =>
+  resolve(
+    __dirname,
+    `../node_modules/vue-demi/lib/${version === 2 ? 'v2.7' : 'v3'}/index.mjs`
+  )
+
 const buildBundle = async (version: 2 | 3) => {
   const plugins: Plugin[] = [version === 2 ? Vue2() : Vue()]
 
   return build({
     build: {
       rollupOptions: {
-        external: ['vue', 'vue-demi'],
+        external: ['vue'],
         treeshake: true,
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
+        },
       },
       lib: {
         entry: resolve(componentsRoot, 'src', 'index.ts'),
-        formats: ['cjs', 'es'],
-        fileName: (format) => `index.${format === 'cjs' ? 'cjs' : 'mjs'}`,
+        name: 'DemiUI',
+        fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
       },
       outDir: resolve(libOutput, 'dist', `v${version}`),
       emptyOutDir: false,
       sourcemap: true,
     },
     plugins,
+    resolve: {
+      alias: {
+        'vue-demi': vueDemiEntry(version),
+      },
+    },
   })
 }
 
--- packages/components/src/button/index.ts
+++ packages/components/src/button/index.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { App, isVue2 } from 'vue-demi'
+import { App } from 'vue-demi'
 import Button from './src/button.vue'
 
 export const DButton = Button
 
 DButton.install = (app: App) => {
-  if (isVue2) {
-    app.component(DButton.name, DButton as any)
-  }
+  app.component(DButton.name, DButton as any)
 }
